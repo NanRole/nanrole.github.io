0cbe2c3fdc7a19f8852b1100a7a0351d8a9f9445
NanRole <104119376+NanRole@users.noreply.github.com>
2025-03-07 02:58:17 +0800

    Update reurl.js

--- reurl_20250307/reurl.js
+++ reurl_20250307/reurl.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const REURL_API_KEY = "4070ff49d794e736185e3b663c974755ecd6be31959f04df8a38b58d65165567c4f5d6";
 const REURL_ENDPOINT = "https://api.reurl.cc";
 
 function reurl_shorten_url(input_url) {
-	return fetch(REURL_ENDPOINT, {
+	return fetch(proxyUrl+REURL_ENDPOINT, {
 	    method: "POST",
 	    headers: {
 	        "Content-Type": "application/json",
