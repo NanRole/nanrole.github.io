471506c98edf95a650a61027fe4ec5deb05831b5
NanRole <104119376+NanRole@users.noreply.github.com>
2025-03-11 12:20:24 +0800

    Update javascript.js

--- reurl_20250307/blockly/javascript.js
+++ reurl_20250307/blockly/javascript.js
@@ -1,5 +1,5 @@
 Blockly.JavaScript['reurl_shorten_url'] = function (block) {
   var url = Blockly.JavaScript.valueToCode(block, 'url', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'reurl_shorten_url('+url+')';
+  var code = 'await reurl_shorten_url('+url+')';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
